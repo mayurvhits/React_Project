e544eaf96bbc8733d7d1887d55469b2cd7bbbd78
Ashvin Rathod <ashvin.vhits@gmail.com>
2022-02-17 18:03:42 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import img1 from './assets/loginMainImg.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex justify-content-around flex-fill">
+        <img src={img1} alt="No Img" width="400" height="400" />
+
+        <Form className="form">
+          <h1 className="h1">Sign in Gilly</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Your email</Form.Label>
+            <Form.Control
+              className="placeholder"
+              type="email"
+              placeholder="name@example.com"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              className="placeholder"
+              type="password"
+              placeholder="Enter the Password"
+            />
+          </Form.Group>
+          <Form.Group
+            className="d-flex justify-content-between "
+            controlId="formBasicCheckbox"
+          >
+            <Form.Check type="checkbox" label="Check me out" />
+            <p className="p2">Lost password?</p>
+          </Form.Group>
+
+          <Button className="button" variant="primary" type="submit">
+            Create account
+          </Button>
+          <fieldset className="d-flex">
+            <p className="p ">Not registered? </p>
+            <p className="p1 ">Creat contact </p>
+          </fieldset>
+        </Form>
+      </div>
     </div>
   );
 }
