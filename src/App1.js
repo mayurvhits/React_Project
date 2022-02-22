b8d3bf2bf8019faad3b2baac8fa3f5f519e94975
Ashvin Rathod <ashvin.vhits@gmail.com>
2022-02-22 18:40:40 +0530

    hi

---
+++ src/App1.js
@@ -0,0 +1,78 @@
+// import './App.css';
+// import { Button, Form } from 'react-bootstrap';
+// import img1 from './assets/loginMainImg.png';
+
+//  function App() {
+   
+
+
+//   return (
+//     <div className="App">
+
+//         <div className='d-flex justify-content-around '>
+
+//         <fieldset>
+//         <img src={img1} alt="No Img" width="400" height="400" />
+//         </fieldset>
+    
+        
+//         <Form className="d-flex flex-sm-column" >
+
+//           <h1 className="h1">Create your Free Account</h1>
+//           <Form.Group className="mb-3" controlId="formBasicEmail">
+//             <Form.Label>Your email</Form.Label>
+//             <Form.Control
+//               className="placeholder"
+//               type="text"
+              
+//               placeholder="name@example.com"
+//             />
+//           </Form.Group>
+
+//           <Form.Group className="mb-3" controlId="formBasicPassword">
+//             <Form.Label>Password</Form.Label>
+//             <Form.Control
+//               className="placeholder"
+//               type="text"
+              
+//               placeholder="Enter the Password"
+//             />
+//           </Form.Group>
+
+//           <Form.Group className="mb-3" controlId="formBasicEmail">
+//             <Form.Label>Confirm Password</Form.Label>
+//             <Form.Control
+//               className="placeholder"
+//               type="text"
+              
+//               placeholder="Enter the confirm password"
+//             />
+//           </Form.Group>
+          
+//           <fieldset className='d-flex justify-content-between'>
+//           <Form.Group
+//             className="d-flex  "
+//             controlId="formBasicCheckbox"
+//           >
+//             <Form.Check className="check" type="checkbox" label="I accept the" />
+//             <a className="p3">Terms and Conditions</a>
+            
+//           </Form.Group>
+//           <a className="p2">Lost password?</a>
+//           </fieldset>
+
+//           <Button className="button" variant="primary" type="submit" >
+//             Create account
+//           </Button>
+//           <fieldset className="d-flex ">
+//             <p className="p ">Already have an account? </p>
+//             <a className="p1 ">Login here </a>
+//           </fieldset>
+//         </Form>
+//       </div>
+//       </div>
+    
+//   );
+// }
+
+// export default App;
