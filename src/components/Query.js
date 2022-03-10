5075d6dac927d4586f24230edc98fd4f689f31ac
Mayur bhalgama <mayur.vhits@gmail.com>
2022-03-10 12:21:02 +0530

    form handling

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,31 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button, Form, FloatingLabel } from 'react-bootstrap';
 
-function Query() {
+class Query extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name:'',
+       email:'',
+       text:'',
+    }
+  }
+
+  handleNameChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handleTextChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    alert(`${this.state.name} ${this.state.email} ${this.state.text}`)
+  }
+ 
+  render(){
   return (
     <div>
       <>
         <div>
-          <Form className="form">
+          <Form className="form" onSubmit={this.handleSubmit}> 
             <h1>Query Section</h1>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>
                 Name
               </Form.Label>
               
-              <Form.Control type="text" placeholder="Full name" />
+              <Form.Control 
+              type="text" 
+              placeholder="Full name" 
+              value={this.state.name}
+              onChange={this.handleNameChange}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Email ID</Form.Label>
-              <Form.Control type="tel" placeholder="Enter your mail" />
+              <Form.Control 
+              type="tel" 
+              placeholder="Enter your mail" 
+              value={this.state.email}
+              onChange={this.handleEmailChange}/>
             </Form.Group>
             <FloatingLabel
               controlId="floatingTextarea"
               label="Ask your queries here"
               className="mb-3"
             >
-              <Form.Control as="textarea" placeholder="Leave a comment here" />
+              <Form.Control 
+              type="textarea" 
+              placeholder="Leave a comment here"
+              value={this.state.text}
+              onChange={this.handleTextChange} />
             </FloatingLabel>
             {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
@@ -38,6 +84,7 @@ function Query() {
       </>
     </div>
   );
+}
 }
 
 export default Query;
